82fdfdd2d129cecbfbe67c910d34b212665c9fc7
whgudfkr <whgudfkr@nate.com>
2014-08-13 01:36:43 +0900

    start project

---
+++ src/test/java/prj/Crawl.java
@@ -0,0 +1,23 @@
+package prj;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawl {
+	public static void main(String[] args) {
+
+		try {
+			Document doc = Jsoup.connect("http://www.naver.com").get();
+			Elements link = doc.select("a");
+			for(Element e : link){
+				String s = e.attr("href");
+				System.out.println(s);
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
+}
