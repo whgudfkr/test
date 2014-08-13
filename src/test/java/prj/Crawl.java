7ffd90123cca2e39f42a11a5762bcb80b9ce7b04
whgudfkr <whgudfkr@nate.com>
2014-08-13 09:54:39 +0900

    adfadasd

--- src/test/java/prj/Crawl.java
+++ src/test/java/prj/Crawl.java
@@ -4,7 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import alsdfkjals;
 
 public class Crawl {
 	public static void main(String[] args) {
